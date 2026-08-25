e24c757636d346e2f9527dbdd61ce2499d535e3f
roberto dias <roberto.dias@memoriacao.local>
2026-08-25 11:59:02 -0300

    roberto dias: atualização do conteúdo do site

---
+++ assets/minus-_cSWv8s2.js
@@ -0,0 +1,1 @@
+import{K as s}from"./index-COsIsBn1.js";const e=s("minus",[["path",{d:"M5 12h14",key:"1ays0h"}]]);export{e as M};
---
+++ assets/plus-BRt41H8x.js
@@ -0,0 +1,1 @@
+import{K as e}from"./index-COsIsBn1.js";const a=e("plus",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]);export{a as P};
